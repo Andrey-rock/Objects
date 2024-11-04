11e05b476e6ddff63936dde6c5070bed00039f2c
Andrey-rock <andrey-rok@maol.ru>
2024-11-04 23:58:46 +0300

    Домашняя работа выполнена

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,17 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        Author author1 = new Author("Фрэнк", "Герберт");
+        Author author2 = new Author("Александр", "Пушкин");
+
+        Book book1 = new Book("Дюна", author1, 1963);
+        Book book2 = new Book("Евнений Онегин", author2, 1833);
+
+        System.out.println(book1.toString());
+        System.out.println(book2.toString());
+
+        book1.setYearPublication(1965);
+
+        System.out.println(book1.toString());
     }
 }
